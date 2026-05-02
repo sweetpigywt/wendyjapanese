710645c4f22be98b2d5c88746b72eee5ac3925ed
Wendy <sweetpigywt@gmail.com>
2026-05-03 00:51:21 +0900

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,22 +1,19 @@
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
+import react from "@vitejs/plugin-react";
 import path from "path";
-import { componentTagger } from "lovable-tagger";
 
-// https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({
-  server: {
-    host: "::",
-    port: 8080,
-    hmr: {
-      overlay: false,
-    },
-  },
-  plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
+export default defineConfig({
+  // 这个 base 选项能帮助浏览器在 GitHub Pages 上正确找到你的网页文件
+  base: "/wendyjapanese/",
+  
+  plugins: [
+    react()
+  ],
+  
+  // 这个 resolve 配置是为了支持项目里的 "@/" 快捷路径寻找文件
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
-    dedupe: ["react", "react-dom", "react/jsx-runtime", "react/jsx-dev-runtime", "@tanstack/react-query", "@tanstack/query-core"],
   },
-}));
+});
