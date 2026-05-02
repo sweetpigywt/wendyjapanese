60a0902ef0fd99a8ce39a0e5f18ab8c4a50e6ab8
Wendy <sweetpigywt@gmail.com>
2026-05-03 01:01:10 +0900

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -19,7 +19,7 @@ const App = () => (
       <TooltipProvider>
         <Toaster />
         <Sonner />
-        <BrowserRouter>
+        <BrowserRouter basename="/wendyjapanese/">
           <Routes>
             <Route path="/" element={<Index />} />
             <Route path="/payments" element={<Payments />} />
